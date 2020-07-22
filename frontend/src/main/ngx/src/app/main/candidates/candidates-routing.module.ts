8124d720a6782cfa533f453be2ce32160090e897
adrianblvi <adrianblancovi@gmail.com>
2020-07-22 13:47:14 +0200

    added detail component to candidates module

--- frontend/src/main/ngx/src/app/main/candidates/candidates-routing.module.ts
+++ frontend/src/main/ngx/src/app/main/candidates/candidates-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CandidatesHomeComponent } from './candidates-home/candidates-home.component';
+import { CandidatesDetailComponent } from './candidates-detail/candidates-detail.component';
 const routes: Routes = [
   {
     path: '',
     component: CandidatesHomeComponent
+  },
+  {
+    path: ':ID',
+    component: CandidatesDetailComponent
   }
 ];
 
