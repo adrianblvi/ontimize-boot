61d508611760b6a4830849e253d25e6d68da43f9
adrianblvi <adrianblancovi@gmail.com>
2020-07-24 13:51:22 +0200

    Added cardview to home component

--- frontend/src/main/ngx/src/app/shared/app.menu.config.ts
+++ frontend/src/main/ngx/src/app/shared/app.menu.config.ts
@@ -2,7 +2,10 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'offers', name: 'OFFERS', icon: 'work', route: '/main/offers' },
-  { id: 'candidates', name: 'CANDIDATES', icon: 'people', route: '/main/candidates' },
+  {
+    id: 'views', name: 'VIEWS', icon: 'remove_red_eye', opened: true,
+    items: [{ id: 'offers', name: 'OFFERS', icon: 'work', route: '/main/offers' },
+    { id: 'candidates', name: 'CANDIDATES', icon: 'people', route: '/main/candidates' }]
+  },
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
